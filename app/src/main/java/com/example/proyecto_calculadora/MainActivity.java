cf102314378c6d41e60ec598ec2bbc538572db48
AndresRicardoSarmiento <66749337+AndresRicardoSarmiento@users.noreply.github.com>
2020-06-27 17:01:44 -0500

    Paso3

--- app/src/main/java/com/example/proyecto_calculadora/MainActivity.java
+++ app/src/main/java/com/example/proyecto_calculadora/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.proyecto_calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_suma;
+    private Button btn_resta;
 
+
+
+    private Button btn_division;
+    private Button btn_multiplicacion;
+
+    private TextView text_respuesta;
+    private EditText edit_num1;
+    private EditText edit_num2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text_respuesta = findViewById(R.id.Respuesta);
+        edit_num1 = findViewById(R.id.num1);
+        edit_num2 = findViewById(R.id.num2);
+        
+
+
     }
 }
