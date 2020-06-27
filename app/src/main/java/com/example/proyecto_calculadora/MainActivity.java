2474216556b11373708026bc302ca8d854f20971
AndresRicardoSarmiento <66749337+AndresRicardoSarmiento@users.noreply.github.com>
2020-06-27 17:15:52 -0500

    Paso8

--- app/src/main/java/com/example/proyecto_calculadora/MainActivity.java
+++ app/src/main/java/com/example/proyecto_calculadora/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.proyecto_calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
